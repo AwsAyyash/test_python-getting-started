3c4c30a4d78190703a660eb539c6e2a2f1816a66
Aws Ayyash <74304034+AwsAyyash@users.noreply.github.com>
2025-12-29 19:33:24 +0200

    Update ALLOWED_HOSTS default value in settings.py
    Changed default value of ALLOWED_HOSTS to 'localhost'.
    
    Signed-off-by: Aws Ayyash <74304034+AwsAyyash@users.noreply.github.com>

--- settings.py
+++ settings.py
@@ -1,3 +1,4 @@
 import os
 
-ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '*').split(',')
+# Add this line to read DJANGO_ALLOWED_HOSTS env var
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', 'localhost').split(',')
